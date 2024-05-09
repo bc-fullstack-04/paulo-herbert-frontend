af015b6b10ea4a9df6606aac75df782d46ff614e
peaga <pauloherbert@alu.ufc.br>
2024-05-08 22:43:19 -0300

    landing home page

--- src/main.tsx
+++ src/main.tsx
@@ -3,27 +3,29 @@ import './global.css';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Page01 } from './pages/Navegation/Page01';
 import { Page02 } from './pages/Navegation/Page02';
 import { ErrorPage } from './pages/Navegation/Error';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { AuthProvider } from './context/AuthContext';
 import { PrivateRoutes } from './utils/PrivateRoutes';
+import { Landing } from './pages/Landing';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.Fragment>
     <Toaster position='top-right' toastOptions={{ duration: 2000 }} />
     <AuthProvider>
-      {/* REACT ROUTER */}
+      {/* REACT ROUTER 
+       <Route path='*' element={<ErrorPage />} />
+          
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/*' element={<PrivateRoutes />}>
+            <Route path='/page02' element={<Page02 />} />
+          </Route>*/}
       <BrowserRouter>
         <Routes>
-          <Route path='*' element={<ErrorPage />} />
-          <Route path='/' index element={<Login />} />
-          <Route path='/signup' element={<Signup/>} />
-          <Route path='' element={<PrivateRoutes />}>
-          </Route>
-          <Route path='/page02' element={<Page02 />} />
+        <Route path='/' index element={<Landing />} />
+          
         </Routes>
       </BrowserRouter>
       {/* REACT ROUTER */}
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,53 +1,33 @@
 import { AlbumModel } from '@/models/AlbumModel';
 import { album_api } from '@/services/apiService';
 import React, { useEffect, useState } from 'react'
+import logo from "../../assets/logo.svg"
+import { Link } from 'react-router-dom';
 
 export function Landing() {
-  const [albums, setAlbums] = useState<AlbumModel[]>([]);
-
-  useEffect(() => {
-    album_api.defaults.headers.common.Authorization = "Basic YWx2ZXNyZW5hbnNhbnRvc0Bob3RtYWlsLmNvbTokMmEkMTAkZU5vblNhaDZHMGEvNnA2T2ZOVk1nZWUxRVlOY2RWOUZHQm5OaHp0azJhYU9odlg3OS80Slc"
-    album_api.get('/albums/all?searchText=Rock')
-    .then((resp) => {
-      setAlbums(resp.data);
-      console.log(albums);
-    })
-  }, []);
-
-
-  function handleLink(url?: string) {
-    window.open(url, '_blank');
-  }
-
   return (
-    <main className="flex flex-col items-center justify-center h-full mt-10 gap-4">
-      <h1 className="text-2xl font-semibold">Albums</h1>
-      <input type="text" />
-      <button>Buscar</button>
-
-      <section className="flex flex-wrap justify-center h-full gap-4 m-2">
-        {/* Card */}
-        { albums?.map((album, i) => (
-          <div key={i} style={{'--bg-fundo': `url(${album.images[0].url})`} as React.CSSProperties} className="bg-[image:var(--bg-fundo)] bg-cover bg-no-repeat w-60 h-[245px] rounded-md">
-            <div onClick={() => handleLink(album.externalUrls.externalUrls.spotify)} className="flex h-full justify-center items-center backdrop-brightness-50 p-6 cursor-pointer">
-              <h1 className="text-2xl font-semibold text-center text-white">{album.name}</h1>
+    <>
+      <main className='bg-fundo h-screen bg-cover bg-no-repeat'>
+        <header className=' px-[100px] py-3 flex justify-between bg-white backdrop-brightness-50 bg-opacity-25 backdrop-blur-xl'>
+            <div className='text-white flex gap-2 items-center'>
+              <img src={logo} alt="logo" />
+              <p>BootPlay</p>
             </div>
-          </div>
-        ))}
-
-        {/* { albums?.map((album, i) => (
-          <div className="w-60 h-[245px] rounded-md">
-            <div className="absolute w-60 h-[245px] backdrop-brightness-50">
-              <div className="flex items-center justify-center w-full h-full p-6">
-                <h1 className="text-2xl font-semibold text-center text-white">{album.name}</h1>
-              </div>
+            <div className='flex gap-3'>
+              <Link to="/login" className="w-[200px] rounded-[32px] bg-[#010B0F] text-white flex justify-center font-semibold items-center ">Entrar</Link>
+              <Link to="/signup" className="w-[200px] rounded-[32px] bg-[#9EE2FF] font-semibold flex justify-center items-center">Inscrever-se</Link>
             </div>
-            <img src={album.images[0].url} alt={album.name} className="w-full h-60 object-cover" />
+        </header>
+        <div className='h-screen backdrop-brightness-50 '>
+          <div className='mx-[100px] w-[701px] text-white pt-[220px]'>
+            <h1 className='font-semibold text-[64px]'>A história da música não pode ser esquecida!</h1>
+            <p className='text-[24px] font-normal text-balance'>Crie já sua conta e curta os sucessos que marcaram os tempos no Vinil.</p>
+            <Link to="/signup" className="mt-3 text-black w-[270px] py-4 rounded-[32px] bg-[#9EE2FF] font-semibold flex justify-center items-center">Inscrever-se</Link>
           </div>
-        )) } */}
-        {/* Card */}
-      </section>
-    </main>
+
+        </div>
+      </main>
+    </>
   )
 }
 
